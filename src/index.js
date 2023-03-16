1d1a51539413bbb25bdb0bd417e779ff2864ea3c
darkseiD779966 <ibrahim.darkseid@gmail.com>
2023-03-16 18:39:19 +0530

    Deployed website

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {HashRouter} from 'react-router-dom';
 import './index.css';
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<React.StrictMode>
+<HashRouter basename={process.env.PUBLIC_URL}>
+    <App />
+</HashRouter>
+</React.StrictMode>,
+document.getElementById('root')
+);
